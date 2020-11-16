c5b9322d27f11454fc92f6c5ed6cda464d8bbc11
CSFelix <57205604+CSFelix@users.noreply.github.com>
2020-11-16 17:06:21 -0300

    Update faceDetection.py

--- Python/faceDetection.py
+++ Python/faceDetection.py
@@ -5,11 +5,13 @@ import os
 facePath = os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(facePath)
 
+# Create 'video' and 'out' objects ('out' is the video that will be saved)
 video = cv2.VideoCapture(0)
 out = cv2.VideoWriter('faceDetection.mp4', -1, 20.0, (640,480))
 
 while True:
     try:
+        # start the video with RGB colors
         ret, frames = video.read()
         gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         
@@ -21,12 +23,14 @@ while True:
         out.write(frames)
         cv2.imshow('WebCam', frames)
     
+    	# press 'q' to break
         if cv2.waitKey(1) & 0xFF == ord('q'): break
         
     except Exception as e:
         print(str(e))
         break
 
+# turn down the video and finish CV2 execution
 video.release()
 out.release()
 cv2.destroyAllWindows()
