e68541329b380b1599d3dadcfc59d26b08cca170
CSFelix <57205604+CSFelix@users.noreply.github.com>
2020-11-16 17:27:03 -0300

    Update eyeDetection.py

--- Python/eyeDetection.py
+++ Python/eyeDetection.py
@@ -5,29 +5,32 @@ import os
 eyePath = os.path.dirname(cv2.__file__)+"/data/haarcascade_eye.xml"
 eyeCascade = cv2.CascadeClassifier(eyePath)
 
-
+# Create 'video' and 'out' objects ('out' is the video that will be saved)
 video = cv2.VideoCapture(0)
 out = cv2.VideoWriter('eyeDetection.mp4', -1, 20.0, (640,480))
 
 while True:
     try:
+        # start the video with RGB colors
         ret, frames = video.read()
         gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         
-        # Eye Detection
+        # Eyes Detection
         eyes = eyeCascade.detectMultiScale(gray)
-        for (x, y, w, h) in eyes: cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 165, 255), 2)
+        for (x, y, w, h) in eyes: cv2.rectangle(frames, (x, y), (x + w, y + h), (255, 255, 0), 2)
         
         # Save and Show the Video
         out.write(frames)
         cv2.imshow('WebCam', frames)
     
+    	# press 'q' to break
         if cv2.waitKey(1) & 0xFF == ord('q'): break
         
     except Exception as e:
         print(str(e))
         break
 
+# turn down the video and finish CV2 execution
 video.release()
 out.release()
 cv2.destroyAllWindows()
